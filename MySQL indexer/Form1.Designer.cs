67aaa243372548609c3aacab7eebfea709647270
Виталий.НПСО <vitaley.usa@gmail.com>
2020-06-12 13:39:29 +0500

    Add project files.

---
+++ MySQL indexer/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace MySQL_indexer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.InputDB = new System.Windows.Forms.TextBox();
+            this.InputServer = new System.Windows.Forms.TextBox();
+            this.InputName = new System.Windows.Forms.TextBox();
+            this.InputPassword = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.year_2019 = new System.Windows.Forms.Label();
+            this.year_2018 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.year_2017 = new System.Windows.Forms.Label();
+            this.year_2016 = new System.Windows.Forms.Label();
+            this.year_2020 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(256, 451);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Показать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // InputDB
+            // 
+            this.InputDB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputDB.Location = new System.Drawing.Point(124, 64);
+            this.InputDB.Name = "InputDB";
+            this.InputDB.Size = new System.Drawing.Size(226, 20);
+            this.InputDB.TabIndex = 3;
+            // 
+            // InputServer
+            // 
+            this.InputServer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputServer.Location = new System.Drawing.Point(124, 89);
+            this.InputServer.Name = "InputServer";
+            this.InputServer.Size = new System.Drawing.Size(226, 20);
+            this.InputServer.TabIndex = 4;
+            // 
+            // InputName
+            // 
+            this.InputName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InputName.Location = new System.Drawing.Point(124, 12);
+            this.InputName.Name = "InputName";
+            this.InputName.Size = new System.Drawing.Size(226, 20);
+            this.InputName.TabIndex = 1;
+            // 
+            // InputPassword
+            // 
+            this.InputPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputPassword.Location = new System.Drawing.Point(124, 38);
+            this.InputPassword.Name = "InputPassword";
+            this.InputPassword.PasswordChar = '*';
+            this.InputPassword.Size = new System.Drawing.Size(226, 20);
+            this.InputPassword.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.BackColor = System.Drawing.SystemColors.Window;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(11, 135);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(339, 310);
+            this.listView1.TabIndex = 5;
+            this.listView1.TabStop = false;
+            this.listView1.TileSize = new System.Drawing.Size(220, 20);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(8, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Имя пользователя";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Gainsboro;
+            this.label2.Location = new System.Drawing.Point(8, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Пароль";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Gainsboro;
+            this.label3.Location = new System.Drawing.Point(8, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "База данных";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Gainsboro;
+            this.label4.Location = new System.Drawing.Point(8, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Сервер";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // year_2019
+            // 
+            this.year_2019.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.year_2019.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.year_2019.Location = new System.Drawing.Point(255, 112);
+            this.year_2019.Name = "year_2019";
+            this.year_2019.Size = new System.Drawing.Size(43, 20);
+            this.year_2019.TabIndex = 15;
+            this.year_2019.Text = "2019";
+            this.year_2019.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.year_2019.Click += new System.EventHandler(this.Year_2019_Click);
+            // 
+            // year_2018
+            // 
+            this.year_2018.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.year_2018.Location = new System.Drawing.Point(206, 112);
+            this.year_2018.Name = "year_2018";
+            this.year_2018.Size = new System.Drawing.Size(43, 20);
+            this.year_2018.TabIndex = 16;
+            this.year_2018.Text = "2018";
+            this.year_2018.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.year_2018.Click += new System.EventHandler(this.Year_2018_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Enabled = false;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(156, 451);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 31);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label5.Location = new System.Drawing.Point(9, 451);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 12);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Ситников Виталий";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label6.Location = new System.Drawing.Point(9, 466);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 12);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Михаил Самсонов";
+            // 
+            // year_2017
+            // 
+            this.year_2017.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.year_2017.Location = new System.Drawing.Point(157, 112);
+            this.year_2017.Name = "year_2017";
+            this.year_2017.Size = new System.Drawing.Size(43, 20);
+            this.year_2017.TabIndex = 16;
+            this.year_2017.Text = "2017";
+            this.year_2017.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.year_2017.Click += new System.EventHandler(this.Year_2017_Click);
+            // 
+            // year_2016
+            // 
+            this.year_2016.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.year_2016.Location = new System.Drawing.Point(108, 112);
+            this.year_2016.Name = "year_2016";
+            this.year_2016.Size = new System.Drawing.Size(43, 20);
+            this.year_2016.TabIndex = 16;
+            this.year_2016.Text = "2016";
+            this.year_2016.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.year_2016.Click += new System.EventHandler(this.Year_2016_Click);
+            // 
+            // year_2020
+            // 
+            this.year_2020.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.year_2020.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.year_2020.Location = new System.Drawing.Point(304, 112);
+            this.year_2020.Name = "year_2020";
+            this.year_2020.Size = new System.Drawing.Size(43, 20);
+            this.year_2020.TabIndex = 15;
+            this.year_2020.Text = "2020";
+            this.year_2020.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.year_2020.Click += new System.EventHandler(this.Year_2020_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(365, 487);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.year_2016);
+            this.Controls.Add(this.year_2017);
+            this.Controls.Add(this.year_2018);
+            this.Controls.Add(this.year_2020);
+            this.Controls.Add(this.year_2019);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.InputPassword);
+            this.Controls.Add(this.InputName);
+            this.Controls.Add(this.InputServer);
+            this.Controls.Add(this.InputDB);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(381, 526);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Пропущенные номера РНД";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox InputDB;
+        private System.Windows.Forms.TextBox InputServer;
+        private System.Windows.Forms.TextBox InputName;
+        private System.Windows.Forms.TextBox InputPassword;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label year_2018;
+        private System.Windows.Forms.Label year_2019;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label year_2017;
+        private System.Windows.Forms.Label year_2016;
+        private System.Windows.Forms.Label year_2020;
+    }
+}
+
---
+++ MySQL indexer/Form1.cs
@@ -0,0 +1,272 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+using System.Windows.Forms;
+
+namespace MySQL_indexer
+{
+    public partial class Form1 : Form
+    {
+        private string Year = "2020";
+        private int _savebutton;
+
+        public int SaveButton
+        {
+            get { return _savebutton; }
+            set
+            {
+                _savebutton = value;
+                if (_savebutton == 0)
+                {
+                    button2.Enabled = false;
+                }
+                else
+                {
+                    button2.Enabled = true;
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < Properties.Settings.Default.PassNumber; i++)
+                Properties.Settings.Default.Password = Properties.Settings.Default.Password.DecryptString();
+
+            Properties.Settings.Default.PassNumber = 0;
+
+            InputName.Text = Properties.Settings.Default.Username;
+            InputPassword.Text = Properties.Settings.Default.Password;
+            InputDB.Text = Properties.Settings.Default.Database;
+            InputServer.Text = Properties.Settings.Default.Server;
+        }
+
+        // Сохраняем настройки
+        private void SaveSettings()
+        {
+            Properties.Settings.Default.PassNumber += 1;
+            Properties.Settings.Default.Password = Properties.Settings.Default.Password.EncryptString();
+            Properties.Settings.Default.Save();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            Properties.Settings.Default.Username = InputName.Text;
+            Properties.Settings.Default.Password = InputPassword.Text;
+            Properties.Settings.Default.Database = InputDB.Text;
+            Properties.Settings.Default.Server = InputServer.Text;
+
+            string Database = InputDB.Text;
+            if (Database == "") Database = "enotdb";
+            string Source = InputServer.Text;
+            if (Source == "") Source = "localhost";
+            string User = InputName.Text;
+            if (User == "") User = "enot";
+
+            string Password = InputPassword.Text;
+            string con = "Server=" + Source + ";Database=" + Database + ";Uid=" + User + ";Pwd=" + Password + ";SslMode=Preferred";
+
+            try
+            {
+                string query = "select Distinct DocDate, RegNumPrefix, RegNumIndex from NotarialActs where YEAR(docDate)='" + Year + "' Order by RegNumPrefix, RegNumIndex";
+
+                MySqlConnection connection = new MySqlConnection(con);
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                connection.Open();
+
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+
+                string[] prefixes = new string[dt.Rows.Count];
+
+                int counter = 0;
+                int counter_prefix = 0;
+                int counter_1time = 0;
+                string prev_prefix;
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int index = Convert.ToInt32(reader["RegNumIndex"]);
+                        string prefix = reader["RegNumPrefix"].ToString();
+
+                        prefixes[counter_prefix] = Convert.ToString(reader["RegNumPrefix"]);
+
+                        if (counter_1time == 0)
+                        {
+                            counter_1time++;
+                            prev_prefix = prefixes[counter_prefix];
+                        }
+                        else
+                        {
+                            prev_prefix = prefixes[counter_prefix - 1];
+                        }
+
+                        if ((prefixes[counter_prefix] != prev_prefix) || (index < counter))
+                            counter = 0;
+
+                        if (index == counter)
+                            counter = index - 1;
+
+                        counter_prefix++;
+
+                        //Console.WriteLine(counter.ToString() + " | " + index.ToString());
+
+                        if (counter + 1 < index) 
+                        {
+                            while (counter + 1 < index)
+                            {
+                                counter++;
+                                //Console.WriteLine("count = " + counter.ToString() );
+                                listView1.Items.Add(prefix + counter);
+                            }
+                        }
+                        counter++;
+                    }
+                    reader.Close();
+                }
+                connection.Close();
+
+                SaveSettings();
+            }
+            catch
+            {
+                MessageBox.Show("Возникла ошибка. Проверьте правильность введеных данных. \n \n");
+            }
+            if (listView1.Items.Count > 0) { SaveButton = 1; }
+        }
+
+        private void Year_2020_Click(object sender, EventArgs e)
+        {
+            Year = "2020";
+            listView1.Clear();
+            year_2016.Font = new Font(year_2016.Font, FontStyle.Regular);
+            year_2017.Font = new Font(year_2017.Font, FontStyle.Regular);
+            year_2018.Font = new Font(year_2018.Font, FontStyle.Regular);
+            year_2019.Font = new Font(year_2019.Font, FontStyle.Regular);
+            year_2020.Font = new Font(year_2020.Font, FontStyle.Bold);
+            SaveButton = 0;
+        }
+
+        private void Year_2019_Click(object sender, EventArgs e)
+        {
+            Year = "2019";
+            listView1.Clear();
+            year_2016.Font = new Font(year_2016.Font, FontStyle.Regular);
+            year_2017.Font = new Font(year_2017.Font, FontStyle.Regular);
+            year_2018.Font = new Font(year_2018.Font, FontStyle.Regular);
+            year_2019.Font = new Font(year_2019.Font, FontStyle.Bold);
+            year_2020.Font = new Font(year_2020.Font, FontStyle.Regular);
+            SaveButton = 0;
+        }
+
+        private void Year_2018_Click(object sender, EventArgs e)
+        {
+            Year = "2018";
+            listView1.Clear();
+            year_2016.Font = new Font(year_2016.Font, FontStyle.Regular);
+            year_2017.Font = new Font(year_2017.Font, FontStyle.Regular);
+            year_2018.Font = new Font(year_2018.Font, FontStyle.Bold);
+            year_2019.Font = new Font(year_2019.Font, FontStyle.Regular);
+            year_2020.Font = new Font(year_2020.Font, FontStyle.Regular);
+            SaveButton = 0;
+        }
+
+        private void Year_2017_Click(object sender, EventArgs e)
+        {
+            Year = "2017";
+            listView1.Clear();
+            year_2016.Font = new Font(year_2016.Font, FontStyle.Regular);
+            year_2017.Font = new Font(year_2017.Font, FontStyle.Bold);
+            year_2018.Font = new Font(year_2018.Font, FontStyle.Regular);
+            year_2019.Font = new Font(year_2019.Font, FontStyle.Regular);
+            year_2020.Font = new Font(year_2020.Font, FontStyle.Regular);
+            SaveButton = 0;
+        }
+
+        private void Year_2016_Click(object sender, EventArgs e)
+        {
+            Year = "2016";
+            listView1.Clear();
+            year_2016.Font = new Font(year_2016.Font, FontStyle.Bold);
+            year_2017.Font = new Font(year_2017.Font, FontStyle.Regular);
+            year_2018.Font = new Font(year_2018.Font, FontStyle.Regular);
+            year_2019.Font = new Font(year_2019.Font, FontStyle.Regular);
+            year_2020.Font = new Font(year_2020.Font, FontStyle.Regular);
+            SaveButton = 0;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count > 0)
+            {
+                string txt = "Пропущенные номера_" + Year + ".txt";
+                StreamWriter sw = new StreamWriter(txt, true);
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    sw.WriteLine(listView1.Items[i].Text);
+                }
+                sw.Close();
+                Process.Start(txt);
+            }
+        }
+    }
+
+    #region Соленье / приправы
+
+    public static class StringSecurityHelper
+    {
+        private static readonly byte[] entropy = Encoding.Unicode.GetBytes("1AQ'&mc %;lq@31*@!)@asd@.,,z2389");
+
+        public static string DecryptString(this string encryptedData)
+        {
+            if (encryptedData == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                byte[] decryptedData = ProtectedData.Unprotect(Convert.FromBase64String(encryptedData), entropy, DataProtectionScope.CurrentUser);
+
+                return Encoding.Unicode.GetString(decryptedData);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static string EncryptString(this string input)
+        {
+            if (input == null)
+            {
+                return null;
+            }
+
+            byte[] encryptedData = ProtectedData.Protect(Encoding.Unicode.GetBytes(input), entropy, DataProtectionScope.CurrentUser);
+
+            return Convert.ToBase64String(encryptedData);
+        }
+    }
+
+    #endregion Соленье / приправы
+}
